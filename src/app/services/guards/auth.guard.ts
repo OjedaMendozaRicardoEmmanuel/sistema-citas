f8402acd86053640ca6359891e3b154c7d4f6120
OjedaMendozaRicardoEmmanuel <19161369@itoaxaca.edu.mx>
2023-05-25 23:25:01 -0600

    Guards

--- src/app/services/guards/auth.guard.ts
+++ src/app/services/guards/auth.guard.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { ApiService } from '../api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private api: ApiService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    const token = this.api.getToken();
+    if (!token) {
+      this.router.navigate(['/login']);
+      return false;
+    }
     return true;
   }
-  
 }
